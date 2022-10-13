977b20ba337c05474f0d444eb2c96899effb0056
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-10-14 00:04:01 +0700

    Variable Validation

--- validation_test.go
+++ validation_test.go
@@ -1,6 +1,7 @@
 package belajar_golang_validation
 
 import (
+	"fmt"
 	"github.com/go-playground/validator/v10"
 	"testing"
 )
@@ -9,5 +10,15 @@ func TestValidation(t *testing.T) {
 	var validate *validator.Validate = validator.New()
 	if validate == nil {
 		t.Error("Validate is nil")
+	}
+}
+
+func TestValidationVariable(t *testing.T) {
+	validate := validator.New()
+	user := "eko"
+
+	err := validate.Var(user, "required")
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
