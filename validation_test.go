4ae286ef2c46c12ee34e448ae30306d2b862a01b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-10-14 00:59:56 +0700

    Validation Errors

--- validation_test.go
+++ validation_test.go
@@ -72,3 +72,25 @@ func TestStruct(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func TestValidationErrors(t *testing.T) {
+	type LoginRequest struct {
+		Username string `validate:"required,email"`
+		Password string `validate:"required,min=5"`
+	}
+
+	validate := validator.New()
+	loginRequest := LoginRequest{
+		Username: "eko",
+		Password: "eko",
+	}
+
+	err := validate.Struct(loginRequest)
+
+	if err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		for _, fieldError := range validationErrors {
+			fmt.Println("error", fieldError.Field(), "on tag", fieldError.Tag(), "with error", fieldError.Error())
+		}
+	}
+}
