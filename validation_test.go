4d67d8702bbd61378a16a936f614ae537758d70c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-10-14 00:30:49 +0700

    Validasi Struct

--- validation_test.go
+++ validation_test.go
@@ -54,3 +54,21 @@ func TestTagParameter(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func TestStruct(t *testing.T) {
+	type LoginRequest struct {
+		Username string `validate:"required,email"`
+		Password string `validate:"required,min=5"`
+	}
+
+	validate := validator.New()
+	loginRequest := LoginRequest{
+		Username: "eko@example.com",
+		Password: "eko1234",
+	}
+
+	err := validate.Struct(loginRequest)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
