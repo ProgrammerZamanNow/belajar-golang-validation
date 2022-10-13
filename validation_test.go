15a339cff1e69699e286909ef526b31bdec76c0a
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-10-14 01:04:22 +0700

    Validasi Cross Fild

--- validation_test.go
+++ validation_test.go
@@ -94,3 +94,23 @@ func TestValidationErrors(t *testing.T) {
 		}
 	}
 }
+
+func TestStructCrossField(t *testing.T) {
+	type RegisterUser struct {
+		Username        string `validate:"required,email"`
+		Password        string `validate:"required,min=5"`
+		ConfirmPassword string `validate:"required,min=5,eqfield=Password"`
+	}
+
+	validate := validator.New()
+	request := RegisterUser{
+		Username:        "eko@example.com",
+		Password:        "eko1234",
+		ConfirmPassword: "eko1234",
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
