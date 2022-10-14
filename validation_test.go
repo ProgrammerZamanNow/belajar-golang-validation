02ce05af400e856ff6ec5ff4ad70b05a34a8ef89
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-10-14 18:29:58 +0700

    Validasi Map

--- validation_test.go
+++ validation_test.go
@@ -217,3 +217,60 @@ func TestBasicCollection(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func TestMap(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+
+	type School struct {
+		Name string `validate:"required"`
+	}
+
+	type User struct {
+		Id        string            `validate:"required"`
+		Name      string            `validate:"required"`
+		Addresses []Address         `validate:"required,dive"`
+		Hobbies   []string          `validate:"required,dive,required,min=3"`
+		Schools   map[string]School `validate:"dive,keys,required,min=2,endkeys,dive"`
+	}
+
+	validate := validator.New()
+	request := User{
+		Id:   "",
+		Name: "",
+		Addresses: []Address{
+			{
+				City:    "",
+				Country: "",
+			},
+			{
+				City:    "",
+				Country: "",
+			},
+		},
+		Hobbies: []string{
+			"Gaming",
+			"Coding",
+			"",
+			"X",
+		},
+		Schools: map[string]School{
+			"SD": {
+				Name: "SD Indonesia",
+			},
+			"SMP": {
+				Name: "",
+			},
+			"": {
+				Name: "",
+			},
+		},
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
